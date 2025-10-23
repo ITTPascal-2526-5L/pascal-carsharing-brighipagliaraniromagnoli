733398dbf00b72b801e765fef6345feb92a6eebb
Pagliaraniz <giacomo.pagliarani.stud@ispascalcomandini.it>
2025-10-23 13:37:08 +0200

    parametrizzata

--- app.py
+++ app.py
@@ -3,9 +3,16 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-
-def hello_world():
+def home():
     return 'Hello World!'
 
+@app.route('/about')
+def about():
+    return 'About this page'
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    return f'User: {username}'
+
 if __name__ == '__main__':
-    app.run()   
+    app.run(debug=True)
